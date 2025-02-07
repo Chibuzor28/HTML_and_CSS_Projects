2b4d0205c5b5b9ff1ebd5334187c5e0b22b32459
Chibuzor Francis <chibuzorfrancis28@gmail.com>
2025-02-07 10:23:04 +0000

    This files contains some of my learning processes.

---
+++ Basic_HTML_and_JavaScrpt1/Basic_JavaScript_1.js
@@ -0,0 +1,11 @@
+alert("Hello world, i am an alert box");
+
+
+	
+	function My_First_Function() 
+	{
+		var string = "Kiss me, i am an Irish!";
+		var result = string.fontcolour("green");
+		document.getElementById("Irish").innerHTML = 
+		result;
+	}
